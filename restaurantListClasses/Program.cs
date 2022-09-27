2bab87671af11ed3904ea7d513c1dc6513ac0e50
Keith Olson <kolson@dmsi.com>
2022-09-27 09:32:27 -0500

    create review

--- restaurantListClasses/Program.cs
+++ restaurantListClasses/Program.cs
@@ -1,4 +1,14 @@
-﻿namespace RestaurantList;
+﻿//Keith Olson 9/27/22
+//Restaurant List
+//Requirements:
+//Load a list of restaurants and their reviews from a file.
+//Save the list of restaurants and their reviews to a file.
+//Print out the list.
+//Add new entries to the list.
+//Update entries on the list.
+//Delete entries from the list.
+
+namespace RestaurantList;
 class Program
 {
     static void Main(string[] args)
@@ -9,7 +19,7 @@ class Program
         RestaurantList list = new RestaurantList();
         do
         {
-            input = GetMenuOption();
+            input = GetMenuOption();//This will always be upper case. To add an additional option, just need to add a case below.
 
             switch (input[0])
             {
@@ -20,7 +30,8 @@ class Program
                     // SaveToFile(fileName, reviews);
                     break;
                 case 'C':
-                    // CreateNewRecord(reviews);
+                    Restaurant r = GetRestaurantFromUser();
+                    list.AddRestaurant(r);
                     break;
                 case 'R':
                     list.Print();
@@ -34,6 +45,9 @@ class Program
                 case 'Q':
                     Console.WriteLine("Good Bye!");
                     break;
+                default: //if input is not one of these options, then it is invalid. 
+                    Console.WriteLine("That is an invalid choice. Please try again.");
+                    break;
             }
         } while (input != "Q");
     }
@@ -41,30 +55,17 @@ class Program
     static string GetMenuOption()
     {
         //Get Menu Options
-        string input;
-        do
-        {
-            Console.WriteLine("What would you like to do?");
-            Console.WriteLine("O: Open the list of restaurants from the file.");
-            Console.WriteLine("S: Save the reviews back into the file. (This will overwrite what is already in the file)");
-            Console.WriteLine("C: Create a new review.");
-            Console.WriteLine("R: Read all restaurant reviews");
-            Console.WriteLine("U: Update a review");
-            Console.WriteLine("D: Delete a review");
-            Console.WriteLine("Q: Quit");
-            input = Console.ReadLine()!.ToUpper(); //guarantees that the input is uppercase
-            if (!IsValid(input))
-            {
-                Console.WriteLine("That was an invalid choice. Please try again.");
-            }
-        } while (!IsValid(input));
-        return input;
-    }
+        Console.WriteLine("What would you like to do?");
+        Console.WriteLine("O: Open the list of restaurants from the file.");
+        Console.WriteLine("S: Save the reviews back into the file. (This will overwrite what is already in the file)");
+        Console.WriteLine("C: Create a new review.");
+        Console.WriteLine("R: Read all restaurant reviews");
+        Console.WriteLine("U: Update a review");
+        Console.WriteLine("D: Delete a review");
+        Console.WriteLine("Q: Quit");
 
-    static bool IsValid(string input)
-    {
-        //check if the first character of the input is a valid option. Only checking the first character to allow the user more flexibility on what to enter.
-        return input[0] == 'O' || input[0] == 'S' || input[0] == 'C' || input[0] == 'R' || input[0] == 'U' || input[0] == 'D' || input[0] == 'Q';
+        string input = Console.ReadLine()!.ToUpper(); //guarantees that the input is uppercase
+        return input;
     }
 
     static RestaurantList LoadData(string fileName)
@@ -102,5 +103,39 @@ class Program
             list = new RestaurantList();
         }
         return list;
+    }
+
+    static Restaurant GetRestaurantFromUser()
+    {
+        string restaurant;
+        do
+        {
+            Console.Write("Enter a restaurant: ");
+            restaurant = Console.ReadLine()!;
+            if (restaurant == "")
+            {
+                Console.WriteLine("You must enter a name for a restaurant");
+            }
+        } while (restaurant == "");
+
+        int rating = GetValidStarRating("Enter a rating between 0 and 5: ");
+
+        return new Restaurant(restaurant, rating);
+    }
+
+    static int GetValidStarRating(string prompt)
+    {
+        //Get a valid rating between 0 and 5
+        int rating;
+        do
+        {
+            Console.Write(prompt);
+            rating = Convert.ToInt32(Console.ReadLine());
+            if (rating < 0 || rating > 5)
+            {
+                Console.WriteLine("Invalid input");
+            }
+        } while (rating < 0 || rating > 5);
+        return rating;
     }
 }
