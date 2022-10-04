48814377f6bc4e0aab5183e2c4abd444978abcd6
Keith Olson <kolson@dmsi.com>
2022-10-04 15:53:32 -0500

    Update and Delete

--- employeeInterface/Program.cs
+++ employeeInterface/Program.cs
@@ -109,11 +109,106 @@ class Program
     }
     private static void UpdateEmployee(List<Employee> employees)
     {
-        Console.WriteLine("Updating");
+        Console.WriteLine("What is the last name of the employee you would like to update?");
+        string name = Console.ReadLine()!;
+        bool found = false;
+        for (int i = 0; i < employees.Count; i++)
+        {
+            Employee e = employees[i];
+            if (e.LastName.ToUpper().Contains(name.ToUpper()))
+            {
+                Console.WriteLine(e);
+                found = true;
+                Console.WriteLine("Is this the employee you would like to update? (Y/N)");
+                string input = Console.ReadLine()!.ToUpper();
+                if (input == "N")
+                {
+                    continue;
+                }
+                Console.WriteLine("What would you like to update?");
+                Console.WriteLine("1: First Name");
+                Console.WriteLine("2: LastName");
+                Console.WriteLine("3: Type");
+                Console.WriteLine("4: Rate");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("What would you like to update the first name to?");
+                        e.FirstName = Console.ReadLine()!;
+                        break;
+                    case 2:
+                        Console.WriteLine("What would you like to update the last name to?");
+                        e.LastName = Console.ReadLine()!;
+                        break;
+                    case 3:
+                        Console.WriteLine("What type of employee would you like to change this to?");
+                        string type = Console.ReadLine()!;
+                        switch (type)
+                        {
+                            case "S":
+                                Console.WriteLine("What is the yearly salary of this employee?");
+                                double salary = Convert.ToDouble(Console.ReadLine());
+                                employees[i] = new SalaryEmployee(e.LastName, e.FirstName, salary);
+                                break;
+                            case "H":
+                                Console.WriteLine("What is the Hourly Rate of this employee?");
+                                double rate = Convert.ToDouble(Console.ReadLine());
+                                employees[i] = new HourlyEmployee(e.LastName, e.FirstName, rate);
+                                break;
+                            default:
+                                employees[i] = new Employee(e.LastName, e.FirstName, type);
+                                break;
+                        }
+                        break;
+                    case 4:
+                        Console.WriteLine("What rate would you like to update to?");
+                        e.SetRate(Convert.ToDouble(Console.ReadLine()));
+                        break;
+                }
+                return;
+            }
+        }
+        if (!found)
+        {
+            Console.WriteLine($"No employee found containing {name} in their last name");
+        }
+        else
+        {
+            Console.WriteLine("Did not update any records");
+        }
     }
     private static void DeleteEmployee(List<Employee> employees)
     {
-        Console.WriteLine("Deleteing");
+        Console.WriteLine("What is the last name of the employee you would like to delete?");
+        string name = Console.ReadLine()!;
+        bool found = false;
+        for (int i = 0; i < employees.Count; i++)
+        {
+            Employee e = employees[i];
+            if (e.LastName.ToUpper().Contains(name.ToUpper()))
+            {
+                Console.WriteLine(e);
+                found = true;
+                Console.WriteLine("Is this the employee you would like to delete? (Y/N)");
+                string input = Console.ReadLine()!.ToUpper();
+                if (input == "N")
+                {
+                    continue;
+                }
+                employees.Remove(e);
+                Console.WriteLine("Successfully Deleted");
+                return;
+            }
+        }
+        if (!found)
+        {
+            Console.WriteLine("No employee found");
+        }
+        else
+        {
+            Console.WriteLine("No records deleted");
+        }
     }
 
     static string GetMenuOption()
