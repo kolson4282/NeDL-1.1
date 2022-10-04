ad8df9ee25dd8dc566048d64ebc9765ac81e8440
Keith Olson <kolson@dmsi.com>
2022-10-04 15:27:13 -0500

    Method Stubs and Menu

--- employeeInterface/Program.cs
+++ employeeInterface/Program.cs
@@ -29,6 +29,68 @@ class Program
         employees.Add(new SalaryEmployee("Last5", "First5", 100000));
 
         PrintList(employees);
+
+        string input;
+        do
+        {
+            input = GetMenuOption();
+            switch (input)
+            {
+                case "S": //Save
+                    Search(employees);
+                    break;
+                case "C": //Create
+                    AddEmployee(employees);
+                    break;
+                case "R": //Read
+                    PrintList(employees);
+                    break;
+                case "U": //Update
+                    UpdateEmployee(employees);
+                    break;
+                case "D": //Delete
+                    DeleteEmployee();
+                    break;
+                case "Q": //Quit
+                    Console.WriteLine("Good Bye!");
+                    break;
+                default: //All other input will be invalid
+                    Console.WriteLine("Invalid Option. Please try again.");
+                    break;
+            }
+        } while (input != "Q");
+
+    }
+
+    private static void Search(List<Employee> employees)
+    {
+        Console.WriteLine("Searching");
+    }
+    private static void AddEmployee(List<Employee> employees)
+    {
+        Console.WriteLine("Adding");
+    }
+    private static void UpdateEmployee(List<Employee> employees)
+    {
+        Console.WriteLine("Updating");
+    }
+    private static void DeleteEmployee()
+    {
+        Console.WriteLine("Deleteing");
+    }
+
+    static string GetMenuOption()
+    {
+        Console.WriteLine("What would you like to do?");
+        Console.WriteLine("R: Print the list of employees");
+        Console.WriteLine("S: Search for an employee by Last Name");
+        Console.WriteLine("C: Add a new employee");
+        Console.WriteLine("U: Update the information for an employee");
+        Console.WriteLine("D: Delete an employee");
+        Console.WriteLine("Q: Quit");
+        string input = Console.ReadLine()!.ToUpper();
+
+        return input;
     }
 
     static void PrintList(List<Employee> list)
