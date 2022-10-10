c80e64ca80336cf791ef01a6a8bebf47bc69708b
Keith Olson <kolson@dmsi.com>
2022-10-10 09:32:43 -0500

    Executive Membership

---
+++ memberships/ExecutiveMembership.cs
@@ -0,0 +1,26 @@
+namespace memberships
+{
+    class ExecutiveMembership : Membership
+    {
+        public double HighCashBackPercent { get; }
+        public ExecutiveMembership(int id, string email, double cost, double lowPercent, double highPercent) : base(id, email, "regular", cost, lowPercent)
+        {
+            HighCashBackPercent = highPercent;
+        }
+
+        public override double CashBack()
+        {
+            double cashBack;
+            if (PurchaseTotal < 1000)
+            {
+                cashBack = PurchaseTotal * CashBackPercent;
+            }
+            else
+            {
+                cashBack = PurchaseTotal * HighCashBackPercent;
+            }
+            PurchaseTotal = 0;
+            return cashBack;
+        }
+    }
+}
--- memberships/Program.cs
+++ memberships/Program.cs
@@ -7,6 +7,7 @@
             List<Membership> members = new List<Membership>();
             members.Add(new RegularMembership(1, "test@regular.com", 50, .01));
             members.Add(new CorporateMembership(2, "test@corporate.com", 100, .1));
+            members.Add(new ExecutiveMembership(2, "test@executive.com", 100, .01, .1));
 
             foreach (Membership member in members)
             {
@@ -15,6 +16,10 @@
                 Console.WriteLine(member);
                 Console.WriteLine($"Cashback for {member.ID} is ${member.CashBack()}");
                 Console.WriteLine(member);
+                member.Purchase(1500);
+                Console.WriteLine(member);
+                Console.WriteLine($"Cashback for {member.ID} is now ${member.CashBack()}");
+
                 Console.WriteLine();
             }
         }
