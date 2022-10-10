924f5868fd8bab57ba51816d6d21291c2a6446ad
Keith Olson <kolson@dmsi.com>
2022-10-10 09:23:28 -0500

    Corporate Membership

---
+++ memberships/CorporateMembership.cs
@@ -0,0 +1,17 @@
+namespace memberships
+{
+    class CorporateMembership : Membership
+    {
+        public CorporateMembership(int id, string email, double cost, double percent) : base(id, email, "regular", cost, percent)
+        {
+
+        }
+
+        public override double CashBack()
+        {
+            double cashBack = CashBackPercent * PurchaseTotal;
+            PurchaseTotal = 0;
+            return cashBack;
+        }
+    }
+}
--- memberships/Program.cs
+++ memberships/Program.cs
@@ -5,15 +5,17 @@
         static void Main()
         {
             List<Membership> members = new List<Membership>();
-            members.Add(new RegularMembership(1, "test", 50, .02));
+            members.Add(new RegularMembership(1, "test@regular.com", 50, .01));
+            members.Add(new CorporateMembership(2, "test@corporate.com", 100, .1));
 
             foreach (Membership member in members)
             {
-                member.Purchase(100);
+                member.Purchase(1000);
                 member.Return(50);
                 Console.WriteLine(member);
                 Console.WriteLine($"Cashback for {member.ID} is ${member.CashBack()}");
                 Console.WriteLine(member);
+                Console.WriteLine();
             }
         }
     }
